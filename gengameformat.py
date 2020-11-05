a89b00ea3ccd50277347ca50f78d0a89ec9f0066
Adam Tunnic <atunnic@outlook.com>
2020-11-05 14:19:58 -0500

    Updated automator
    Now takes "game.png" as the input icon and renames it to "CUSAxxxxx_00.png".

--- gengameformat.py
+++ gengameformat.py
@@ -3,19 +3,34 @@
 # process of adding games to the games.txt file. Up to 10 games
 # can be added at once.
 
+import os
+import shutil
 loopNUMraw = input("How many games are you planning on adding? (max:10) ")
 loopNUM = int(loopNUMraw)
 a = 10
 b = 0
 while a >= loopNUM > b:
+#	Let's gather the gameID and gameNAME and set our variables for later.
 	gameID = input("Type the game identifier here: ")
 	gameNAME = input("Type the game's full name here: ")
-
 	print("")
+#	Showing you how it's supposed to look.
 	print(gameID + "_00 " + "#" + " " + gameNAME)
+	input("This is what you typed. Press any key to confirm.")
 	loopNUM = loopNUM - 1
 	print("")
+#	Begin appending the game.txt file.
 	file1 = open("games.txt","a")
 	file1.write(gameID + "_00 " + "#" + " " + gameNAME)
 	file1.write("\n")
-input("Here's what I just added to your game.txt file. Press any key to exit.")
+#	Asks for the games.png file to copy into the /ps4 folder.
+	print("Drag you game's icon into this folder and rename it to game.png.")
+	input("Press any key to continue.")
+	old_file_name = "game.png"
+	new_file_name = ("ps4/" + gameID + "_00.png")
+	os.rename(old_file_name, new_file_name)
+	print("")
+#	Showing you that it's done copying.
+	print("We just moved the icon into the right spot with the right name.")
+#	Final notes
+	input("This automator finished the copy process sucessfully. Press any key to exit.")
